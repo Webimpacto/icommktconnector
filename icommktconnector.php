d5b4ec49c500ccff35b9afa6bc095d3ab8e0812e
Iván Gallego <igallego@webimpacto.es>
2019-07-30 14:03:44 +0200

    update module version

--- icommktconnector.php
+++ icommktconnector.php
@@ -26,7 +26,7 @@ class Icommktconnector extends Module
     {
         $this->name = 'icommktconnector';
         $this->tab = 'emailing';
-        $this->version = '1.0.8';
+        $this->version = '1.0.9';
         $this->author = 'icommkt';
         $this->need_instance = 0;
 
